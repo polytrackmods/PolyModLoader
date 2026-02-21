39265b34467e222fa17fda104b9346050e25fb14
0rangy <orangywastaken@gmail.com>
2026-02-21 14:31:09 -0500

    f off caching

--- 1.2.4/main.mod.js
+++ 1.2.4/main.mod.js
@@ -1080,7 +1080,7 @@ class PMLCoreMod extends PolyMod {
     this.modPmlInstance = pmlInstance;
     console.log(`Hello from ${this.modName}!`);
     this.modPmlInstance.registerFuncMixin(
-      "Jc",
+      "Jc", 
       MixinType.INSERT,
       `(0, C.GG)(this, Vc, [], "f");`,
       `
